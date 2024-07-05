62e148db14a4435abbbad94de8fbe7db16513c91
Roman Makeev <makeevrserg@gmail.com>
2024-07-05 19:40:52 +0300

    fix build with krates

--- modules/features/theme/impl/src/commonMain/kotlin/com/makeevrserg/applicationtemplate/mobile/features/theme/data/repository/ThemeSwitcherRepository.kt
+++ modules/features/theme/impl/src/commonMain/kotlin/com/makeevrserg/applicationtemplate/mobile/features/theme/data/repository/ThemeSwitcherRepository.kt
@@ -1,9 +1,9 @@
 package com.makeevrserg.applicationtemplate.mobile.features.theme.data.repository
 
 import com.makeevrserg.applicationtemplate.mobile.features.theme.data.model.Theme
-import ru.astrainteractive.klibs.kstorage.api.StateFlowMutableKrate
+import ru.astrainteractive.klibs.kstorage.api.StateFlowKrate
 
 internal interface ThemeSwitcherRepository {
 
-    val themeFlowStorageValue: StateFlowMutableKrate<Theme>
+    val themeFlowStorageValue: StateFlowKrate.Mutable<Theme>
 }
