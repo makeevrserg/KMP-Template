882ceeecf462c6009c3cf841fb2e7812d9b51498
Roman Makeev <makeevrserg@gmail.com>
2025-12-21 23:15:33 +0300

    fix splash

--- instances/androidApp/src/androidMain/kotlin/examplepackage/MainActivity.kt
+++ instances/androidApp/src/androidMain/kotlin/examplepackage/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.ExperimentalAnimationApi
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.arkivanov.decompose.defaultComponentContext
@@ -17,10 +13,6 @@ import examplepackage.features.root.di.RootModule
 import examplepackage.features.screen.ui.RootScreenContent
 import examplepackage.features.theme.ui.ApplicationTheme
 
-@ExperimentalMaterialApi
-@ExperimentalComposeUiApi
-@ExperimentalAnimationApi
-@ExperimentalFoundationApi
 class MainActivity : ComponentActivity() {
     private val rootModule: RootModule
         get() = application.asMyApplication().rootModule
--- modules/services/core/ui/common/src/commonMain/kotlin/examplepackage/core/ui/components/bottomsheet/SlotModalBottomSheet.kt
+++ modules/services/core/ui/common/src/commonMain/kotlin/examplepackage/core/ui/components/bottomsheet/SlotModalBottomSheet.kt
@@ -1,19 +1,9 @@
 package examplepackage.core.ui.components.bottomsheet
 
-import androidx.compose.foundation.layout.ColumnScope
-import androidx.compose.foundation.layout.WindowInsets
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ModalBottomSheet
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.arkivanov.decompose.router.slot.ChildSlot
-import examplepackage.core.ui.components.bottomsheet.util.zero
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.drop
 
 @Composable
 fun <T : Any, K : Any> SlotModalDialog(
